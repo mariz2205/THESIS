a272bdb497027b5c78905d0ce82345421fb5455e
JASTEENN <djustine446@gmail.com>
2026-09-12 22:41:16 +0800

    angular na

---
+++ app/src/app/features/emergency/calls/calls.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calls',
+  imports: [],
+  templateUrl: './calls.html',
+  styleUrl: './calls.css'
+})
+export class Calls {
+
+}
